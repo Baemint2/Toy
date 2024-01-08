b3687ac407f1450e14dffe8283ddbfd02750da07
Baemint2 <qofndlwl@gmail.com>
2024-01-08 20:31:41 +0900

    20240108_SBB

---
+++ SBB/SBB/src/main/java/org/moz1mozi/sbb/answer/AnswerRepository.java
@@ -0,0 +1,7 @@
+package org.moz1mozi.sbb.answer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
+}
---
+++ SBB/SBB/src/test/java/org/moz1mozi/sbb/SbbApplicationTests.java
@@ -0,0 +1,34 @@
+package org.moz1mozi.sbb;
+
+import static
+		org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.moz1mozi.sbb.answer.Answer;
+import org.moz1mozi.sbb.answer.AnswerRepository;
+import org.moz1mozi.sbb.question.QuestionRepository;
+import org.moz1mozi.sbb.question.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.security.auth.Subject;
+import java.util.Optional;
+
+@SpringBootTest
+class SbbApplicationTests {
+
+	@Autowired
+	private QuestionService questionService;
+
+	@Test
+	void testJpa() {
+		for (int i = 1; i < 300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String content = "냉무";
+			this.questionService.create(subject, content, null);
+		}
+
+		}
+		}
+
